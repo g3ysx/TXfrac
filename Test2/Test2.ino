39454bb1e24444162d4efb18fede6a55e4897bf9
Stewart Bryant <stewart@g3ysx.org.uk>
2022-02-05 14:13:33 +0000

    First fully functional system - not sure if I need to test for high or low, but this approach watches the untilzation gradually rise from 0 to 100%
    Current sample rate is 0.5 sec.

--- Test2/Test2.ino
+++ Test2/Test2.ino
@@ -8,7 +8,20 @@ unsigned long ct; // current time
 unsigned long nt; // next time
 int ctr = 0;
 char buf[3];
-char buf2[30];
+char buf2[300];
+
+const int PttPin = 4;
+
+#define NUMSAMP 60*6*2
+int sample[NUMSAMP];
+#define NEXTTIME 500
+
+int sampleTot = 0;
+int nextSamp = 0;
+int samp;
+
+float fract = 0.0;
+int fractI = 0;
 
 void setup()
 {
@@ -19,23 +32,52 @@ void setup()
   
   nt = millis() + 1000;
   Serial.begin(115200);
+
+  pinMode(PttPin, INPUT);
+  for (int i=0; i<NUMSAMP; i++)
+     sample[i]=0;
+  
 }
 
 void loop()
 {
   ct = millis();
   if (ct < nt) return;
+  nt = nt + NEXTTIME;
 
-  nt = nt + 1000;
+  if (digitalRead(PttPin) == HIGH) //Tx active
+     samp = 1;
+  else
+     samp = 0;
+  sampleTot = sampleTot - sample[nextSamp] + samp;
+  sample[nextSamp] = samp;
+  nextSamp++;
+  if (nextSamp == NUMSAMP)
+     nextSamp=0;
 
-  ctr = ctr + 10;
-  if (ctr>100) ctr = 0;
+  fract = (((float)sampleTot)*100.0)/((float)NUMSAMP);
+  sprintf(buf2, "---sampleTot = %d fract %f NS %d\n", sampleTot, fract, NUMSAMP);
+  Serial.write(buf2);
+  
+  
+  //fract = ((float)sampleTot*100.0)/(float)NUMSAMP;
+  //sprintf(buf2,"---%d %f %f\n",sampleTot, (float)NUMSAMP, fract);
+  //Serial.write(buf2);
+  
+  
+  
+  fractI = (int)(fract + 0.5);
+  sprintf(buf2,"-- %f %d\n",fract, fractI);
+  Serial.write(buf2);
+  
+  //ctr = ctr + 10;
+  //if (ctr>100) ctr = 0;
 
 // u8x8.clearDisplay();
-  sprintf(buf, "%3d", ctr);
+  sprintf(buf, "%3d", fractI);
   u8x8.drawString(0, 0, buf);
 
   // diag - show how much time this took
-  sprintf(buf2,"%d msec\n",millis()-ct);
+  sprintf(buf2,"%d msec %d %d\n",millis()-ct, sampleTot, fractI);
   Serial.write(buf2);
 }
